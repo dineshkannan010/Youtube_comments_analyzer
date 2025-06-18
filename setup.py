6324c495763b7e1d60c8753561882bc5ab8c19f2
Dinesh12 <dineshkannan010@gmail.com>
2025-06-18 00:40:58 -0400

    Greated the base pipeline for App

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'youtube',
+    version= '0.0.0',
+    author= 'Dinesh',
+    author_email= 'dineshkannan010@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
