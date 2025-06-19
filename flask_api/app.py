348f548eeb7124f6109bf258fc18c9c87e71bebf
Dinesh12 <dineshkannan010@gmail.com>
2025-06-19 04:06:37 -0400

    Added code for model section of pipeline

---
+++ flask_api/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import io
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import mlflow
+import numpy as np
+import re
+import pandas as pd
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from mlflow.tracking import MlflowClient
+import matplotlib.dates as mdates
+import pickle
+
+app=Flask(__name__)
+CORS(app)
+
+# Define the preprocessing function
+def preprocess_comment(comment):
+    """Apply preprocessing transformations to a comment."""
+    try:
+        # Convert to lowercase
+        comment = comment.lower()
+
+        # Remove trailing and leading whitespaces
+        comment = comment.strip()
+
+        # Remove newline characters
+        comment = re.sub(r'\n', ' ', comment)
+
+        # Remove non-alphanumeric characters, except punctuation
+        comment = re.sub(r'[^A-Za-z0-9\s!?.,]', '', comment)
+
+        # Remove stopwords but retain important ones for sentiment analysis
+        stop_words = set(stopwords.words('english')) - {'not', 'but', 'however', 'no', 'yet'}
+        comment = ' '.join([word for word in comment.split() if word not in stop_words])
+
+        # Lemmatize the words
+        lemmatizer = WordNetLemmatizer()
+        comment = ' '.join([lemmatizer.lemmatize(word) for word in comment.split()])
+
+        return comment
+    except Exception as e:
+        print(f"Error in preprocessing comment: {e}")
+        return comment
+    
+
+
+
